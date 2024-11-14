0ece6b3cdb5b6af53482bdc49ec54e28ba2b4bec
harsh verma <harsh>
2024-11-14 19:13:15 +0530

    commit

---
+++ server/app.js
@@ -0,0 +1,29 @@
+    const express = require("express")
+    const cors = require("cors")
+    const cookieParser = require("cookie-parser")
+    const bodyParser = require('body-parser');
+    const path = require("path");
+    const app = express()
+
+    const _dirname = path.dirname("")
+    const buildpath = path.join(_dirname , "../client/dist")
+    app.use(express.static(buildpath))
+
+
+    app.use(cors({
+        // origin: 'https://news-application-3rqa.vercel.app',
+        origin : 'http://localhost:5173',
+        credentials: true
+    }));
+
+    app.use(cookieParser())
+    app.use(bodyParser.json());
+
+    app.use(express.json())
+    app.use(express.urlencoded({extended:true}))
+
+    app.use("/" , require("./routes/user.route.js"))
+    app.use("/" , require("./routes/article.route.js"))
+    app.use("/" , require("./routes/comment.route.js"))
+    
+    module.exports = app;
