04a3ec51444a3623038ac7e3d83de4def0dd3eea
harsh verma <harsh>
2024-11-16 02:50:06 +0530

    app.js

--- server/app.js
+++ server/app.js
@@ -1,28 +1,40 @@
-    const express = require("express")
-    const cors = require("cors")
-    const cookieParser = require("cookie-parser")
-    const bodyParser = require('body-parser');
-    const path = require("path");
-    const app = express()
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const path = require("path");
+const app = express();
 
-    const _dirname = path.dirname("")
-    const buildpath = path.join(_dirname , "../client/dist")
-    app.use(express.static(buildpath))
+const connectDataBase = require("./database/index.js");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env" });
 
+const _dirname = path.dirname("");
+const buildpath = path.join(_dirname, "../client/dist");
+app.use(express.static(buildpath));
 
-    app.use(cors({
-        origin : 'http://localhost:5174',
-        credentials: true
-    }));
+app.use(
+  cors({
+    origin: "http://localhost:5174",
+    credentials: true,
+  })
+);
 
-    app.use(cookieParser())
-    app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(bodyParser.json());
 
-    app.use(express.json())
-    app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-    app.use("/" , require("./routes/user.route.js"))
-    app.use("/" , require("./routes/article.route.js"))
-    app.use("/" , require("./routes/comment.route.js"))
-    
-    module.exports = app;
+const PORT = process.env.PORT;
+connectDataBase().then(() => {
+  app.listen(PORT, () => {
+    console.log("http:localhost:4444");
+  });
+});
+
+app.use("/", require("./routes/user.route.js"));
+app.use("/", require("./routes/article.route.js"));
+app.use("/", require("./routes/comment.route.js"));
+
+module.exports = app;
